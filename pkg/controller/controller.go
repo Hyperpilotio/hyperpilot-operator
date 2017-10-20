e3ec4a9d6327073150986d7c435734bab074a2f8
Jimmy Chuang <jimmy@hyperpilot.io>
2017-10-20 14:31:12 -0700

    add k8s deployment yaml add initial event handler

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -9,7 +9,7 @@ import (
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/cache"
 	"log"
-	"reflect"
+	//"reflect"
 	"sync"
 	"time"
 )
@@ -53,12 +53,28 @@ func NewPodController(kclient *kubernetes.Clientset, opts map[string]string) *Po
 		cache.Indexers{},
 	)
 	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		// can get  all existing pod when operator start
+		// and
+		// get new pod with pending status
 		AddFunc: func(cur interface{}) {
-			podWatcher.doTheMagic(cur)
+			podWatcher.getAddInfo(cur)
 		},
+		DeleteFunc: func(cur interface{}) {
+			podWatcher.getDelInfo(cur)
+		},
+		// get event when update, Pending -> Running  -> Successed
+		//                                           |
+		//                                            -> Failed
 		UpdateFunc: func(old, cur interface{}) {
-			if !reflect.DeepEqual(old, cur) {
-				podWatcher.doTheMagic(cur)
+			//if !reflect.DeepEqual(old, cur) {
+			//	podWatcher.getUpdateInfo(old, cur)
+			//}
+
+			oldPod := old.(*v1.Pod)
+			newPod := cur.(*v1.Pod)
+			if oldPod.Status.Phase == "Pending" &&
+				newPod.Status.Phase =="Running"{
+				podWatcher.getUpdateInfo(old, cur)
 			}
 		},
 	})
@@ -108,4 +124,39 @@ func (c *PodController) doTheMagic(cur interface{}) {
 		var jo metav1.DeleteOptions
 		c.kclient.BatchV1Client.Jobs(createdMeta.Reference.Namespace).Delete(createdMeta.Reference.Name, &jo)
 	}
+}
+
+func (c *PodController) getAddInfo(cur interface{})  {
+	podObj := cur.(*v1.Pod)
+
+	if (podObj.Status.Phase == "Running"){
+		log.Printf("Existing Pod")
+		log.Printf("\t Pod: %s, \t NameSpace: %s, \t host: %s\n", podObj.Name ,podObj.Namespace, 	podObj.Spec.NodeName)
+	}else if(podObj.Status.Phase == "Pending"){
+		log.Printf("Newly crete Pod (in Pending Status)")
+		log.Printf("\t Pod: %s, \t NameSpace: %s, \t host: %s \n", podObj.Name ,podObj.Namespace,	podObj.Spec.NodeName)
+	}else if (podObj.Status.Phase == "Succeeded"){
+		log.Printf("Completed Pod (in Successed Status)")
+		log.Printf("\t Pod: %s, \t NameSpace: %s, \t host: %s \n", podObj.Name ,podObj.Namespace, podObj.Spec.NodeName)
+	}else{
+		log.Printf("Not defined Phase: %s", podObj.Status.Phase)
+	}
+}
+
+func (c *PodController) getDelInfo(cur interface{}){
+	podObj := cur.(*v1.Pod)
+	log.Printf("Deleting Pod")
+	log.Printf("\t Pod: %s, \t NameSpace: %s, \t Status: %s \n",
+		podObj.Name ,podObj.Namespace, podObj.Status.Phase)
+}
+
+func (c *PodController) getUpdateInfo(old, cur interface{})  {
+	oldObj := old.(*v1.Pod)
+	newObj := cur.(*v1.Pod)
+	log.Printf("Newly crete Pod (in Running Status)")
+	if oldObj.Status.Phase == "Pending" &&
+		newObj.Status.Phase =="Running"{
+		log.Printf("\t Pod: %s, \t NameSpace: %s, \t host: %s \n", newObj.Name ,newObj.Namespace,	newObj.Spec.NodeName)
+	}
+
 }
