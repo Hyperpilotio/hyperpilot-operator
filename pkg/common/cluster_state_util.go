534e61761ac5849c92e22b8427f05908c65e4faf
Jimmy Chuang <jimmy@hyperpilot.io>
2017-11-22 17:01:21 -0800

    add locks for shared objects

--- pkg/common/cluster_state_util.go
+++ pkg/common/cluster_state_util.go
@@ -4,10 +4,15 @@ import (
 	"errors"
 	"log"
 	"strings"
+	"sync"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
+
+const HYPERPILOT_OPERATOR_NS = ""
 
 type NodeInfo struct {
 	NodeName   string
@@ -19,9 +24,75 @@ type ClusterState struct {
 	Nodes       map[string]NodeInfo
 	Pods        map[string]*v1.Pod
 	ReplicaSets map[string]*v1beta1.ReplicaSet
+	Lock        *sync.RWMutex
+}
+
+func NewClusterState() *ClusterState {
+	return &ClusterState{
+		Nodes:       make(map[string]NodeInfo),
+		Pods:        make(map[string]*v1.Pod),
+		ReplicaSets: make(map[string]*v1beta1.ReplicaSet),
+		Lock:        &sync.RWMutex{},
+	}
+}
+
+func (clusterState *ClusterState) PopulateNodeInfo(kclient *kubernetes.Clientset) error {
+	nodes, err := kclient.Nodes().List(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	clusterState.Lock.Lock()
+	defer clusterState.Lock.Unlock()
+
+	for _, n := range nodes.Items {
+		a := NodeInfo{
+			NodeName:   n.Name,
+			ExternalIP: n.Status.Addresses[1].Address,
+			InternalIP: n.Status.Addresses[0].Address,
+		}
+		clusterState.Nodes[a.NodeName] = a
+	}
+	return nil
+}
+
+func (clusterState *ClusterState) PopulatePods(kclient *kubernetes.Clientset) error {
+	pods, err := kclient.Pods(HYPERPILOT_OPERATOR_NS).List(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	clusterState.Lock.Lock()
+	defer clusterState.Lock.Unlock()
+
+	for _, p := range pods.Items {
+		pod := p
+		clusterState.Pods[pod.Name] = &pod
+	}
+
+	return nil
+}
+
+func (clusterState *ClusterState) PopulateReplicaSet(kclient *kubernetes.Clientset) error {
+	rss, err := kclient.ReplicaSets(HYPERPILOT_OPERATOR_NS).List(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	clusterState.Lock.Lock()
+	defer clusterState.Lock.Unlock()
+
+	for _, r := range rss.Items {
+		rs := r
+		clusterState.ReplicaSets[rs.Name] = &rs
+	}
+	return nil
 }
 
 func (clusterState *ClusterState) FindReplicaSetHash(deploymentName string) (string, error) {
+	clusterState.Lock.RLock()
+	defer clusterState.Lock.RUnlock()
+
 	for _, v := range clusterState.ReplicaSets {
 		hash := v.ObjectMeta.Labels["pod-template-hash"]
 		for _, owner := range v.OwnerReferences {
@@ -34,6 +105,9 @@ func (clusterState *ClusterState) FindReplicaSetHash(deploymentName string) (str
 }
 
 func (clusterState *ClusterState) FindDeploymentPod(namespace, deploymentName, hash string) []*v1.Pod {
+	clusterState.Lock.RLock()
+	defer clusterState.Lock.RUnlock()
+
 	r := []*v1.Pod{}
 	for podName, pod := range clusterState.Pods {
 		if strings.HasPrefix(podName, deploymentName) && strings.Contains(podName, hash) && pod.Namespace == namespace {
@@ -48,6 +122,9 @@ func (clusterState *ClusterState) FindDeploymentPod(namespace, deploymentName, h
 }
 
 func (clusterState *ClusterState) FindStatefulSetPod(namespace, statefulSetName string) []*v1.Pod {
+	clusterState.Lock.RLock()
+	defer clusterState.Lock.RUnlock()
+
 	r := []*v1.Pod{}
 	for _, pod := range clusterState.Pods {
 		for _, own := range pod.OwnerReferences {
